822ecc0bae549feaea6d4cfb605b3fe1530f3318
James Andrei <jamesocampo@gmail.com>
2024-02-23 13:02:31 +0800

    updated signupfragment, response

--- app/src/main/java/com/kristeljeanne/bautista/block1/p1/buyinselloutadmin/SignupFragment.kt
+++ app/src/main/java/com/kristeljeanne/bautista/block1/p1/buyinselloutadmin/SignupFragment.kt
@@ -1,7 +1,9 @@
 package com.kristeljeanne.bautista.block1.p1.buyinselloutadmin
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.JsonReader
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -32,9 +34,9 @@ class SignupFragment : Fragment() {
         binding = FragmentSignupBinding.inflate(inflater, container, false)
 
         binding.signupButton.setOnClickListener{
-            val name = binding.fullname.text.toString()
-            val email = binding.email.text.toString()
-            val password = binding.password.text.toString()
+            val name = binding.fullname.text.toString().trim()
+            val email = binding.email.text.toString().trim()
+            val password = binding.password.text.toString().trim()
 
             //json data
             val signupDataJson =
@@ -59,19 +61,35 @@ class SignupFragment : Fragment() {
                 reader.beginObject()
                 reader.close()
                 RetrofitClient.instance.createUser(name, email, password).enqueue(object : Callback<DefaultResponse> {
-                    override fun onResponse(call: Call<DefaultResponse>, response: Response<DefaultResponse>) {
+                    override fun onResponse(call: Call<DefaultResponse>, response: retrofit2.Response<DefaultResponse>) {
                         // Handle response
-                        Toast.makeText(applicationContext, response.body()?.message, Toast.LENGTH_LONG).show()
+                        if (response.isSuccessful && response.body() != null) {
+                            Toast.makeText(requireContext(), response.body()!!.message, Toast.LENGTH_LONG).show()
+                            startActivity(Intent(requireActivity(), LoginFragment::class.java))
+                        }
+                        else {
+                            val errorMessage: String = try {
+                                response.errorBody()?.string()
+                                    ?: "Failed to get a valid response. Response code: ${response.code()}"
+                            } catch (e: Exception) {
+                                "Failed to get a valid response. Response code: ${response.code()}"
+                            }
+                            Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_LONG)
+                                .show()
+                            Log.e("API_RESPONSE", errorMessage)
+                        }
                     }
 
                     override fun onFailure(call: Call<DefaultResponse>, t: Throwable) {
                         // Handle failure
-                        Toast.makeText(applicationContext, t.message, Toast.LENGTH_LONG).show()
+                        Toast.makeText(requireContext(), t.message, Toast.LENGTH_LONG).show()
                     }
                 })
 
-                }
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
+
 
         }
         return binding.root
--- app/src/main/java/com/kristeljeanne/bautista/block1/p1/buyinselloutadmin/model/Response.kt
+++
@@ -1,5 +0,0 @@
-package com.kristeljeanne.bautista.block1.p1.buyinselloutadmin.model
-
-data class Response(
-
-)
